3b21d14c834370d8752e2a8f9c0de5ba1d2d8be0
aclash <aclash@163.com>
2020-03-04 20:33:46 -0500

    212

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Wait till the browser is ready to render the game (avoids glitches)
       new GameManager(4, KeyboardInputManager, HTMLActuator);
   });
   
